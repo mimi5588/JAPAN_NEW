38767940fd34d851a99404f909bc5287c2694839
may cohen <maycohen5588@gmail.com>
2026-08-11 16:24:40 +0300

    Set final site link and refresh cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'japan-trip-v3';
+const CACHE_NAME = 'japan-trip-v4';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
 
 self.addEventListener('install', event => {
