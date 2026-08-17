95c1d1c8182e3e0ddfb3f20ca427335f8681091c
may cohen <maycohen5588@gmail.com>
2026-08-17 17:45:20 +0300

    Deploy Hebrew shopping and cache cleanup

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = 'japan-trip-disabled-v11';
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(key => caches.delete(key))))
+      .then(() => self.registration.unregister())
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', event => {
+  return;
+});
